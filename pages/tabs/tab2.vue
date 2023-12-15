6c6c8558927dc5aa63f35a9db3c7d16add5c81a6
Paul <52328887+paulbraam@users.noreply.github.com>
2023-12-15 17:52:06 +0300

    #1 Add multiplatform

---
+++ pages/tabs/tab2.vue
@@ -0,0 +1,12 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Tab 2</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+      Tab 2 content
+    </ion-content>
+  </ion-page>
+</template>
