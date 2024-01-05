4d2f402e3a62893a8e3079e105b7a827b6260748
Paul <52328887+paulbraam@users.noreply.github.com>
2024-01-05 22:43:48 +0300

    #9 Add navigation

---
+++ pages/index/trainings.vue
@@ -0,0 +1,17 @@
+<template>
+  <ion-page>
+    <ion-content>
+      <div class="flex h-full">
+        <div class="my-auto w-full flex justify-center">
+          Here is supposed to be a list of trainings
+        </div>
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  middleware: 'auth'
+});
+</script>
