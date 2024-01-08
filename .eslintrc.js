20bfba3fa850986ee95108fb373f98b94e6ba7ea
Paul <52328887+paulbraam@users.noreply.github.com>
2024-01-08 18:29:38 +0300

    #10 Fix training linter issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,6 @@
 /** @type {import("eslint").Linter.Config} */
 const config = {
+  root: true,
   parser: 'vue-eslint-parser',
   parserOptions: {
     parser: '@typescript-eslint/parser',
@@ -18,7 +19,7 @@ const config = {
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-type-checked',
     'plugin:@typescript-eslint/stylistic-type-checked',
-    'plugin:prettier/recommended'
+    // 'plugin:prettier/recommended'
   ],
   settings: {
     'import/resolver': {
@@ -28,77 +29,99 @@ const config = {
       }
     }
   },
-  rules: {
-    // @typescript-eslint
-    '@typescript-eslint/no-unsafe-enum-comparison': 'off',
-    '@typescript-eslint/restrict-template-expressions': 'off',
-    "@typescript-eslint/no-unsafe-member-access": "off",
-    '@typescript-eslint/no-var-requires': 1,
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    '@typescript-eslint/array-type': 'off',
-    '@typescript-eslint/consistent-type-definitions': 'off',
-    '@typescript-eslint/no-unsafe-assignment': 'off',
-    '@typescript-eslint/no-unsafe-call': 'off',
-    '@typescript-eslint/consistent-type-imports': [
-      'warn',
-      {
-        prefer: 'type-imports',
-        fixStyle: 'inline-type-imports'
-      }
-    ],
-    '@typescript-eslint/no-misused-promises': 'off',
-    // import
-    'import/no-cycle': 'off',
-    'import/no-anonymous-default-export': 'off',
-    'import/no-extraneous-dependencies': 'off',
-    'import/prefer-default-export': 'off',
-    'import/extensions': ['error', 'never', { css: 'always', json: 'always', vue: 'always' }],
-    'import/order': [
-      'error',
-      {
-        groups: ['external', 'index', 'sibling', 'parent', 'builtin', 'object', 'type', 'internal']
-      }
-    ],
-    // prettier
-    'prettier/prettier': [
-      'error',
-      {
-        singleQuote: true,
-        trailingComma: 'none',
-        jsxBracketSameLine: false,
-        endOfLine: 'lf',
-        jsxSingleQuote: false,
-        printWidth: 100,
-        arrowParens: 'always',
-        tabWidth: 2
-        // plugins: ['prettier-plugin-tailwindcss'],
-        // tailwindFunctions: ['clsx']
-      }
-    ],
-    // vue
-    'vue/multi-word-component-names': 'off',
-    'vue/no-deprecated-slot-attribute': 'off',
-    // common
-    '@typescript-eslint/no-empty-function': 'off',
-    'comma-dangle': [
-      'error',
-      {
-        arrays: 'never',
-        objects: 'never',
-        imports: 'never',
-        exports: 'never',
-        functions: 'never'
-      }
-    ],
-    'no-shadow': 'off',
-    camelcase: 'off',
-    'no-unused-vars': 'off',
-    'max-classes-per-file': 'off',
-    'no-param-reassign': 'off',
-    'func-names': 'off',
-    'no-undef': 'off',
-    'consistent-return': 'off'
-  }
+  //   rules: {
+  //     // @typescript-eslint
+  //     '@typescript-eslint/no-unsafe-enum-comparison': 'off',
+  //     '@typescript-eslint/restrict-template-expressions': 'off',
+  //     "@typescript-eslint/no-unsafe-member-access": "off",
+  //     '@typescript-eslint/no-var-requires': 1,
+  //     '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+  //     '@typescript-eslint/array-type': 'off',
+  //     '@typescript-eslint/consistent-type-definitions': 'off',
+  //     '@typescript-eslint/no-unsafe-assignment': 'off',
+  //     '@typescript-eslint/no-unsafe-call': 'off',
+  //     '@typescript-eslint/consistent-type-imports': [
+  //       'warn',
+  //       {
+  //         prefer: 'type-imports',
+  //         fixStyle: 'inline-type-imports'
+  //       }
+  //     ],
+  //     '@typescript-eslint/no-misused-promises': 'off',
+  //     // import
+  //     'import/no-cycle': 'off',
+  //     'import/no-anonymous-default-export': 'off',
+  //     'import/no-extraneous-dependencies': 'off',
+  //     'import/prefer-default-export': 'off',
+  //     'import/extensions': ['error', 'never', { css: 'always', json: 'always', vue: 'always' }],
+  //     'import/order': [
+  //       'error',
+  //       {
+  //         groups: ['external', 'index', 'sibling', 'parent', 'builtin', 'object', 'type', 'internal']
+  //       }
+  //     ],
+  //     // prettier
+  //     'prettier/prettier': [
+  //       'error',
+  //       {
+  //         singleQuote: true,
+  //         trailingComma: 'none',
+  //         jsxBracketSameLine: false,
+  //         endOfLine: 'lf',
+  //         jsxSingleQuote: false,
+  //         printWidth: 100,
+  //         arrowParens: 'always',
+  //         tabWidth: 2
+  //         // plugins: ['prettier-plugin-tailwindcss'],
+  //         // tailwindFunctions: ['clsx']
+  //       }
+  //     ],
+  //     // vue
+  //     'vue/multi-word-component-names': 'off',
+  //     'vue/no-deprecated-slot-attribute': 'off',
+  //     // common
+  //     '@typescript-eslint/no-empty-function': 'off',
+  //     'comma-dangle': [
+  //       'error',
+  //       {
+  //         arrays: 'never',
+  //         objects: 'never',
+  //         imports: 'never',
+  //         exports: 'never',
+  //         functions: 'never'
+  //       }
+  //     ],
+  //     'no-shadow': 'off',
+  //     camelcase: 'off',
+  //     'no-unused-vars': 'off',
+  //     'max-classes-per-file': 'off',
+  //     'no-param-reassign': 'off',
+  //     'func-names': 'off',
+  //     'no-undef': 'off',
+  //     'consistent-return': 'off'
+  //   }
 };
 
+// /** @type {import("eslint").Linter.Config} */
+// const config = {
+//   root: true,
+//   parser: 'vue-eslint-parser',
+//   parserOptions: {
+//     parser: '@typescript-eslint/parser',
+//     sourceType: 'module'
+//   },
+//   extends: [
+//     '@nuxtjs/eslint-config-typescript',
+//     'plugin:vue/vue3-recommended',
+//     // 'plugin:import/errors',
+//     // 'plugin:import/warnings',
+//     // 'plugin:import/typescript',
+//     // 'plugin:@typescript-eslint/recommended',
+//     // 'plugin:@typescript-eslint/recommended-type-checked',
+//     // 'plugin:@typescript-eslint/stylistic-type-checked',
+//     // 'plugin:prettier-vue/recommended'
+//   ]
+// };
+
 module.exports = config;
+
--- pages/index/trainings/new/components/TrainingForm/TrainingForm.vue
+++ pages/index/trainings/new/components/TrainingForm/TrainingForm.vue
@@ -1,31 +1,35 @@
 <template>
-  <form @submit.prevent="onSubmit" :novalidate="false">
+  <form :novalidate="true" @submit="onSubmit">
     <ion-list :inset="true">
       <ion-item>
-        <ion-input id="at" v-model="at" type="datetime-local" label="Date"></ion-input>
+        <ion-input id="at" v-model="at" type="datetime-local" label="Date" />
         <ion-modal trigger="at" :initial-breakpoint="0.35" :breakpoints="[0, 0.35, 1]">
           <ion-content class="ion-padding">
             <ion-datetime
               presentation="date-time"
               :prefer-wheel="true"
               :show-default-buttons="true"
-              @ion-change="onDatetimeChange" 
-            ></ion-datetime>
-          </ion-content>
+              @ion-change="onDatetimeChange"
+            />
+          </ion-content>     
         </ion-modal>
       </ion-item>
       <ion-item>
-        <ion-input v-model="weight" type="number" label="Weight (kg)"> </ion-input>
-        <ion-note v-if="errors.weight" color="danger">{{ errors.weight }}</ion-note>
+        <ion-input v-model="weight" type="number" label="Weight (kg)" />
+        <ion-note v-if="errors['userParams.weight']" color="danger">
+          {{ errors['userParams.weight'] }}
+        </ion-note>
       </ion-item>
       <ion-item>
-        <ion-input v-model="heartRate" type="number" label="Heart rate"></ion-input>
-        <ion-note v-if="errors.heartRate" color="danger">{{ errors.heartRate }}</ion-note>
+        <ion-input v-model="heartRate" type="number" label="Heart rate" />
+        <ion-note v-if="errors['userParams.heartRate']" color="danger">
+          {{ errors['userParams.heartRate'] }}
+        </ion-note>
       </ion-item>
-      <ion-item> 
+      <ion-item>
         <ion-button class="w-full" size="default" :disabled="isLoading" type="submit">
-          <ion-spinner v-if="isLoading" name="crescent"></ion-spinner>
-          <span v-else>Create</span>  
+          <ion-spinner v-if="isLoading" name="crescent" />
+          <span v-else>Create</span>
         </ion-button>
       </ion-item>
     </ion-list>
@@ -33,52 +37,60 @@
 </template>
 
 <script setup lang="ts">
-import { IonInput, toastController } from '@ionic/vue';
-import dayjs from 'dayjs';
-import { z } from 'zod';
-import type { DatetimeCustomEvent } from '@ionic/vue';
+import { IonInput, toastController } from '@ionic/vue'
+import dayjs from 'dayjs'
+import { z } from 'zod'
+import type { DatetimeCustomEvent } from '@ionic/vue'
 import type {
   CreateUserTrainingRequestBody,
   CreateUserTrainingResponse
-} from '~/server/api/trainings/types';
+} from '~/server/api/trainings/types'
 
-const router = useIonRouter();
+const router = useIonRouter()
 
-const { value: at } = useField('training.at');
-const { value: weight } = useField('userParams.weight');
-const { value: heartRate } = useField('userParams.heartRate');
-
-const validationSchema = toTypedSchema<z.ZodType<CreateUserTrainingRequestBody>>(z.object({
-  training: z.object({
-    at: z.union([z.string(), z.date()])
-  }), 
-  userParams: z.object({
-    weight: z.number().min(1, { message: 'Too short' }),
-    heartRate: z.number().min(1, { message: 'Too short' })
+const validationSchema = toTypedSchema<z.ZodType<CreateUserTrainingRequestBody>>(
+  z.object({
+    training: z.object({
+      at: z.union([z.string(), z.date()])
+    }),
+    userParams: z.object({
+      weight: z.number().min(1, { message: 'Too short' }),
+      heartRate: z.number().min(1, { message: 'Too short' })
+    })
   })
-}));
+)
 
-const { handleSubmit, values, setFieldValue, errors } = useForm({
-  validationSchema 
-});
+const { handleSubmit, values, setFieldValue, errors, defineField, resetForm } = useForm({
+  validationSchema
+})
 
-// const body = computed<CreateUserTrainingRequestBody>(() => ({
-//   training: {
-//     at: new Date(at.value ?? '').toISOString()
-//   },
-//   userParams: {
-//     weight: Number(weight.value),
-//     heartRate: Number(heartRate.value)
-//   }
-// }));
+const [at] = defineField('training.at')
+const [weight] = defineField('userParams.weight')
+const [heartRate] = defineField('userParams.heartRate')
 
-console.log(errors.value); 
+watch(
+  () => values,
+  params => console.log(params.userParams),
+  {
+    deep: true
+  }
+)
+
+watch(
+  () => errors,
+  errors => console.log('errors', errors.value),
+  {
+    deep: true
+  }
+)
 
 const onDatetimeChange = (event: DatetimeCustomEvent) => {
-  const date = event.detail.value;
-  const isString = typeof date === 'string';
-  if (isString) setFieldValue('training.at', dayjs(date).format('YYYY-MM-DDTHH:mm'));
-};
+  const date = event.detail.value
+  const isString = typeof date === 'string'
+  if (isString) {
+    setFieldValue('training.at', dayjs(date).format('YYYY-MM-DDTHH:mm'))
+  }
+}
 
 const { execute, status, error } = await useFetch<CreateUserTrainingResponse>('/api/trainings', {
   method: 'POST',
@@ -87,17 +99,15 @@ const { execute, status, error } = await useFetch<CreateUserTrainingResponse>('/
   watch: false,
   onResponse: ({ response }) => {
     if (response.ok) {
-      // eslint-disable-next-line no-underscore-dangle
-      const training: CreateUserTrainingResponse = response._data;
-      router.push(`/trainings/${training.id}`); 
+      const training: CreateUserTrainingResponse = response._data
+      router.push(`/trainings/${training.id}`)
     }
   }
-}); 
+})
 
-const onSubmit = handleSubmit(async (values) => {
-  console.log(values); 
-  // await execute();
-});
+const onSubmit = handleSubmit(async () => {
+  await execute()
+})
 
 watch(
   () => error.value,
@@ -108,22 +118,20 @@ watch(
         duration: 1500,
         color: 'danger',
         position: 'top'
-      });
+      })
 
-      await toast.present();
+      await toast.present()
     }
   }
-);
+)
 
-const isLoading = computed(() => status.value === 'pending');
+const isLoading = computed(() => status.value === 'pending')
 
 onIonViewDidLeave(() => {
-  at.value = null;
-  weight.value = null;
-  heartRate.value = null;
-});
+  resetForm()
+})
 
 definePageMeta({
   middleware: 'auth'
-});
+})
 </script>
