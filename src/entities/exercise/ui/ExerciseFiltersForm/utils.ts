45063ea37c30e38219b94e595d687d91101ee5a5
Paul <52328887+paulbraam@users.noreply.github.com>
2024-03-23 22:19:45 +0300

    #53 Fix validation errors

--- src/entities/exercise/ui/ExerciseFiltersForm/utils.ts
+++ src/entities/exercise/ui/ExerciseFiltersForm/utils.ts
@@ -4,9 +4,21 @@ import type { ExerciseFiltersForm } from './types';
 
 export const exerciseFiltersFormValidationSchema = toTypedSchema<z.ZodType<ExerciseFiltersForm>>(
   z.object({
-    muscle: z.union([z.nativeEnum(ExerciseMuscle), z.literal('')]),
-    category: z.union([z.nativeEnum(ExerciseCategory), z.literal('')]),
-    equipment: z.union([z.nativeEnum(ExerciseEquipment), z.literal('')]),
-    force: z.union([z.nativeEnum(ExerciseForce), z.literal('')])
+    muscle: z
+      .union([z.nativeEnum(ExerciseMuscle), z.literal('')])
+      .optional()
+      .transform((value) => value || ''),
+    category: z
+      .union([z.nativeEnum(ExerciseCategory), z.literal('')])
+      .optional()
+      .transform((value) => value || ''),
+    equipment: z
+      .union([z.nativeEnum(ExerciseEquipment), z.literal('')])
+      .optional()
+      .transform((value) => value || ''),
+    force: z
+      .union([z.nativeEnum(ExerciseForce), z.literal('')])
+      .optional()
+      .transform((value) => value || '')
   })
 );
